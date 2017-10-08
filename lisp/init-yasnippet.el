0fc1624cce0f859d529ff85b60ac0daff6f432c3
Anderlang <itbjm@sina.com>
2017-10-09 00:04:40 +0800

    add snippet

---
+++ lisp/init-yasnippet.el
@@ -0,0 +1,12 @@
+;;----------------------
+;;输入模板
+;;----------------------
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode 1)
+  )
+
+(provide 'init-yasnippet)
+
+;;; init-yasnippet.el ends here
