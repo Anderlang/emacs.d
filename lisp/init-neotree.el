0e9cf74b25b06e5521eac9c8c40e3b06bb974521
Anderlang <itbjm@sina.com>
2018-02-25 17:54:20 +0800

    添加gtags

---
+++ lisp/init-neotree.el
@@ -0,0 +1,14 @@
+;;;package --- init-neotree
+;;; Commentary:目录树
+;;; 
+;;; Code:
+(use-package neotree
+  :ensure t
+  )
+
+(global-set-key (kbd "C-c t") 'neotree-toggle)
+
+
+(provide 'init-neotree)
+
+;;; init-neotree ends here
